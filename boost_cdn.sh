2e54e268dd898aa197a71afc1a969bdf2237925c
vagrant <vagrant@localhost.localdomain>
2017-05-26 05:47:55 +0000

    add boost_cdn.sh

---
+++ boost_cdn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Alternative CDN site 
+cat > mapping <<EOF
+fonts.googleapis.com         fonts.lug.ustc.edu.cn
+ajax.googleapis.com          ajax.lug.ustc.edu.cn
+themes.googleusercontent.com google-themes.lug.ustc.edu.cn
+fonts.gstatic.com            fonts-gstatic.lug.ustc.edu.cn
+EOF
+
+while read line 
+do
+    regexp=${line%% *}
+    replacement=${line##* }
+    find theme/ -type f -print | xargs -i sed -i "s/$regexp/$replacement/" {}
+done < mapping
+
+rm mapping
