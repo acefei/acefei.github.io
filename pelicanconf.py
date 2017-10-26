eff447c54a4de807b9923842ed2ea0c0419563ae
acefei <acefeilxy@gmail.com>
2017-10-26 09:27:47 +0000

    update main page

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,9 +44,13 @@ LANDING_PAGE_ABOUT = {
         'details': """
         <div>
             <h1>欢迎来到Acefei的博客!</h1>
-            <p>我是一名开发攻城狮，有很强的学习能力，并喜欢钻研、思考问题，而且善于总结经验，平时喜欢篮球、电影和游戏。</p>
-            <p>由于<a href='http://blog.csdn.net/csdnproduct/article/details/70145129'>特殊原因</a>(呵呵！)，我的CSDN博客(<a href='http://blog.csdn.net/ace_fei'>ace_fei</a>)已经停止更新！</p>
+            <p>写博客目的:</p>
+            <p>    ——未经你思考的知识是不属于你的。</p>
+            <p>    ——要拒绝思维懒惰, 要勇于发现问题，勤于分析问题，善于解决问题。</p>
+            <p>    ——好记忆不如烂笔头，定期梳理自己的掌握的技术，并记录下来。</p>
+            <p>题外话：由于<a href='http://blog.csdn.net/csdnproduct/article/details/70145129'>特殊原因</a>(呵呵！看评论)，我的CSDN博客(<a href='http://blog.csdn.net/ace_fei'>ace_fei</a>)已经停止更新！</p>
             <hr class="eye-protector-processed" style="transition: background-color 0.3s ease; border-color: rgba(0, 0, 0, 0.35); color: rgb(0, 0, 0); background-color: rgb(193, 230, 198);">
+
             <h2>博文分类</h2>
             <ul>
             <li>DevOps：持续交付、自动化 </li>
@@ -54,9 +58,6 @@ LANDING_PAGE_ABOUT = {
             <li>Misc：意趣、闲思、杂七杂八</li>
             </ul>
             <p>翻阅博客：<a href="/categories.html">文章归档</a> <a href="/tags.html">话题标签</a></p>
-            <h2>电影让你更懂人生</h2>
-            <p>&emsp;&emsp;——人生只能活一次，但是透过电影可以了解到世界上大小人物的人生故事，而这当中，一定有些是说中你心声的故事。</p>
-            <p>近期影单：<a href='http://www.dunkirkmovie.com/'>《DUNKIRK》</a></p>
         </div>
         """
         }
@@ -78,8 +79,8 @@ SOCIAL_PROFILE_LABEL = u'Stay in Touch'
 
 # you can find the icons in http://fontawesome.io/icons/#brand
 SOCIAL = (
-        ('GitHub', 'https://github.com/acefei'),
-        ('Email', 'mailto:acefei@163.com'),
+        ('github', 'https://github.com/acefei'),
+        ('linkedin', 'https://www.linkedin.com/in/fei-su-2b6b10104'),
           )
 
 
